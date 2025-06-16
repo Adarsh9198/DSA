6b513eb94266d5cde44122a2f96d3aaba09fee0b
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-06-16 14:56:16 +0530

    Time: 7 ms (58.20%) | Memory: 48.3 MB (76.27%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -16,25 +16,52 @@
 
 //ab hum isme next greatest element towards right dekh rhe toh RightMax array mei store krlenge aur two pointer approach use karenge
 //T.C=O(n) //every element will be visited once  S.C=O(n)
+
+// class Solution {
+// public:
+//     int maxWidthRamp(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>RightMax(n);
+//         RightMax[n-1]=nums[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             RightMax[i]=max(RightMax[i+1],nums[i]);
+//         }
+//         int i=0;
+//         int j=0;
+//         int ramp_size=0;
+//         while(j<n){
+//             while(i<j && nums[i]>RightMax[j]){//matlab kbhi nums[i]<=nums[j] hit nhi hoga isliye i++            
+//                 i++;
+//         }
+//         ramp_size=max(ramp_size,j-i);
+//         j++;
+//      }
+//      return ramp_size;
+//     }
+// };
+
+
+//T.C=O(2*N)~=O(N) since every element would be popped or pushed only once
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>RightMax(n);
-        RightMax[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            RightMax[i]=max(RightMax[i+1],nums[i]);
+        int n = nums.size();
+        stack<int> st; // monotonic decreasing stack of indices
+        // Step 1: Build the stack of decreasing values' indices
+        for (int i = 0; i < n;i++) {
+            if (st.empty() || nums[i] < nums[st.top()]) {
+                st.push(i);
+            }
         }
-        int i=0;
-        int j=0;
-        int ramp_size=0;
-        while(j<n){
-            while(i<j && nums[i]>RightMax[j]){//matlab kbhi nums[i]<=nums[j] hit nhi hoga isliye i++            
-                i++;
+        int ramp_size = 0;
+        // Step 2: Traverse from end to start to find max ramp width
+        for (int j = n - 1; j >= 0; j--) {
+            while (!st.empty() && nums[j] >= nums[st.top()]) {
+                int i=st.top();
+                ramp_size = max(ramp_size, j-i);
+                st.pop();
+            }
         }
-        ramp_size=max(ramp_size,j-i);
-        j++;
-     }
-     return ramp_size;
+        return ramp_size;
     }
 };
