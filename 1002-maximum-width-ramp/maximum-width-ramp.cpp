48ea09696bf426176584317e10884c02480d1380
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-06-16 14:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (58.57%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+//     int maxWidthRamp(vector<int>& nums) {
+//         int n=nums.size();
+//         int ramp_size=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){//we can traverse j from n-1 to 0,as soon as we get nums[i]<=nums[j] we store the result and break since we wan't maximum j;
+//                 if(nums[i]<=nums[j]){
+//                     ramp_size=max(ramp_size,j-i);
+//                 }
+//             }
+//         }
+//         return ramp_size;
+//     }
+// };
+
+//ab hum isme next greatest element towards right dekh rhe toh RightMax array mei store krlenge aur two pointer approach use karenge
+//T.C=O(n) //every element will be visited once  S.C=O(n)
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>RightMax(n);
+        RightMax[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            RightMax[i]=max(RightMax[i+1],nums[i]);
+        }
+        int i=0;
+        int j=0;
+        int ramp_size=0;
+        while(j<n){
+            while(i<j && nums[i]>RightMax[j]){//matlab kbhi nums[i]<=nums[j] hit nhi hoga isliye i++            
+                i++;
+        }
+        ramp_size=max(ramp_size,j-i);
+        j++;
+     }
+     return ramp_size;
+    }
+};
