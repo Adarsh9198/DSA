92a5f3152328c05373f5dd68a1323ee96ec3d641
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-06-18 16:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (88.52%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -4,15 +4,21 @@ public:
         int n=nums.size();
         int maxVal=*max_element(nums.begin(),nums.end());
         vector<int>sum(maxVal+1,0);
+        // sum[i] = total points milenge agar hum i ko select karte hain
+        // jaise agar 2 baar 3 aaya hai toh sum[3] = 3+3 = 6
         //[2,2,3,3,3,4]=sum_of_each->[0,0,4,9,4]
         for(int i=0;i<n;i++){
             sum[nums[i]]+=nums[i];
         }
-
+        // agar hum sum[i] choose karte hain toh sum[i-1] ko skip karna padega
+        // dp[i] = i tak ke max points
         vector<int>dp(maxVal+1,0);
         dp[0]=sum[0];
         dp[1]=max(sum[0],sum[1]);
         for(int i=2;i<=maxVal;i++){
+            // do options:
+            // 1. current i lo -> sum[i] + dp[i-2]
+            // 2. current i na lo -> dp[i-1]
             dp[i]=max(sum[i]+dp[i-2],dp[i-1]);
         }
         return dp[maxVal];
