4145bdda23c626fd7ddc41df7a522dbebc0d60a5
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-06-26 23:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (91.72%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    typedef pair<int,int>p;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n=arr.size();
-        vector<int>result;
-        priority_queue<p,vector<p>>pq;
-        for(int i=0;i<n;i++){
-            int diff=abs(x-arr[i]);
-            pq.push({diff,arr[i]});
-            if(pq.size()>k) pq.pop();
+        int left=0;
+        int right=arr.size()-1;
+        while(right-left>=k){//K size ki window chahiye isliye
+            if(abs(arr[left]-x)>arr[right]-x){
+                left++;
+            }
+            else{
+                right--;
+            }
         }
-        while(pq.size()>0){
-            int num=pq.top().second;
-            result.push_back(num);
-            pq.pop();
-        }
-        sort(result.begin(),result.end());
-        return result;
+        //arr.begin()+left=>start index, arr.begin()+left+k=> excluding last one
+     return vector<int>(arr.begin()+left,arr.begin()+left+k);
     }
 };
