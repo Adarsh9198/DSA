35fb759a1c6bcb6472bafb76266163e0aeea8425
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-06-29 18:47:55 +0530

    Time: 27 ms (84.84%) | Memory: 54.4 MB (14.79%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int Modulo=1e9+7;
+    int numSubseq(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int count =0;
+        int right=n-1;
+        int left=0;
+        vector<int>pow(n);
+        pow[0]=1;
+        for(int i=1;i<n;i++){
+            pow[i]=(pow[i-1]*2)%Modulo;
+        }
+        while(left<=right){
+            if(nums[left]+nums[right]<=target){
+                count = (count + pow[right - left]) % Modulo;
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return count;
+    }
+};
