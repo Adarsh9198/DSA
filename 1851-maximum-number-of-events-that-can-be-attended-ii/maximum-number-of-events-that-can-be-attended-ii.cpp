6a0b126872deaffc7b619aa497ea5bea05f4d883
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-07-08 10:28:01 +0530

    Time: 151 ms (60.32%) | Memory: 123.6 MB (51.85%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     long long n;
+    int getNext(vector<vector<int>>& events,int l,int currJobEnd){
+        int r=n-1;
+        int result=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(events[mid][0]>currJobEnd){
+                result=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return result;
+    }
     int solve(vector<vector<int>>& events,int idx,int k,vector<vector<int>> &dp){
         if(idx>=n || k==0){
             return 0;
@@ -11,16 +26,18 @@ public:
         int value=events[idx][2];
         int skip=solve(events,idx+1,k,dp);
         //linear search-->T.C=>(n+1)(k+1)* O(n)
-        int j=idx+1;
-        for(;j<events.size();j++){
-            if(events[j][0]>events[idx][1]) break;
-        }
+        // int j=idx+1;
+        // for(;j<events.size();j++){
+        //     if(events[j][0]>events[idx][1]) break;
+        // }
 
         // using upper_bound fn i.e binary search  to find idx of evnt with start>end
         // vector<int>temp={end,INT_MAX,INT_MAX};
         // int j=upper_bound(events.begin()+idx+1,events.end(),temp) -events.begin();
         // idx pr khade h toh uske baad se start kiya.....aur subtract kiya kyoki idx chahiye 
-        int take=value +solve(events,j,k-1,dp);
+
+        int NextIdx=getNext(events,idx+1,events[idx][1]);
+        int take=value +solve(events,NextIdx,k-1,dp);
 
         return dp[idx][k]=max(skip,take);
     }
