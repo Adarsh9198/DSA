e4eb7076fe897909fb823e22b4f6b5c39c332b92
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-07-18 09:51:17 +0530

    Time: 132 ms (29.18%) | Memory: 96.4 MB (52.43%) - LeetSync

---
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        //k se modulo lena hai toh 0 se k-1 remainder aa skta hai
+        //row 0=>(nums[j]+nums[i])==0
+        //row 1=>(nums[j]+nums[i])==1
+        //row 2=>(nums[j]+nums[i])==2.....and so on
+        vector<vector<int>>dp(k,vector<int>(n,1));
+        int maxLen=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+               int mod= (nums[j]+nums[i])%k;
+
+               dp[mod][i]=max(dp[mod][i],dp[mod][j]+1);
+               maxLen=max(maxLen,dp[mod][i]);
+            }
+        }
+        return maxLen;
+    }
+};
