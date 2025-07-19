de600bebef61ff80591d97364c1689b9420d952c
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-07-19 12:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (5.33%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,18 +1,57 @@
+// class Solution {
+// public:
+//     void DFS(unordered_map<int, vector<int>>&adj,int u,vector<bool> &visited){
+//         visited[u]=true;
+//         for(int &v:adj[u]){
+//             if(!visited[v]){
+//                 DFS(adj,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n=isConnected.size();
+//         vector<bool> visited(n,false);
+//         //make graph..
+//         unordered_map<int, vector<int>>adj;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 DFS(adj,i,visited);
+//                 count++;//jaise hi ek province coverkr lega count badh jaayega
+//             }
+//         }
+//         return count;
+//     }
+// };
+//T.C=>O(V+U)
+
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>>&adj,int u,vector<bool> &visited){
+    void BFS(unordered_map<int,vector<int>>&adj,int u,vector<bool>&visited){
+        queue<int>que;
+        que.push(u);
         visited[u]=true;
-        for(int &v:adj[u]){
-            if(!visited[v]){
-                DFS(adj,v,visited);
+        while(!que.empty()){
+            int curr=que.front();
+            que.pop();
+            for(int &v:adj[curr]){
+                if(!visited[v]){
+                    BFS(adj,v,visited);
+                }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<bool> visited(n,false);
-        //make graph..
-        unordered_map<int, vector<int>>adj;
+        unordered_map<int,vector<int>>adj;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1){
@@ -21,11 +60,12 @@ public:
                 }
             }
         }
+        vector<bool>visited(n,false);
         int count=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                DFS(adj,i,visited);
-                count++;//jaise hi ek province coverkr lega count badh jaayega
+                BFS(adj,i,visited);
+                count++;
             }
         }
         return count;
