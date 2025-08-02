ccc3d6e2066f8907265f1b55dfbc408baa0b6565
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-08-02 15:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (56.86%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    //Dijkstra Algorithm Problem
+    typedef pair<long,long>P;
+    const int M=1e9+7;
+    int countPaths(int n, vector<vector<int>>& roads) {
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto &road:roads){
+            int u=road[0];
+            int v=road[1];
+            int time=road[2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
+        }
+        priority_queue<P,vector<P>,greater<P>>pq;//min heap
+        vector<long long>result(n,LLONG_MAX);//pehle sab pr pahucne ka dist infinite le liye
+        vector<int>pathCount(n,0);
+        result[0]=0;
+        pathCount[0]=1;//0 se 0 jaane ka 1 way
+        pq.push({0,0});
+        while(!pq.empty()){
+            long long currTime=pq.top().first;
+            int currNode=pq.top().second;
+            pq.pop();
+            for(auto &vec:adj[currNode]){
+                int adjNode  = vec.first;
+                int roadTime = vec.second;
+                if (currTime + roadTime < result[adjNode]) {
+                    result[adjNode] = currTime + roadTime;
+                    pathCount[adjNode] = pathCount[currNode];
+                    pq.push({result[adjNode], adjNode});
+                } else if (currTime + roadTime == result[adjNode]) {
+                    pathCount[adjNode] = (pathCount[adjNode] + pathCount[currNode]) % M;
+                }
+            }
+        }
+        return pathCount[n-1];
+    }
+};
