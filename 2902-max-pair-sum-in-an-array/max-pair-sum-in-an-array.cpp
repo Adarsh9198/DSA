4716017f4233206933874d28112e213c6981f1f6
Adarsh9198 <83626934+Adarsh9198@users.noreply.github.com>
2025-09-28 13:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 73.5 MB (70.79%) - LeetSync

--- 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.cpp
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.cpp
@@ -1,29 +1,53 @@
+// class Solution {
+// public:
+//     int maxSum(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> max_d(n);
+//         for (int i = 0; i < n; i++) {
+//             int m = 0;
+//             int temp_num = nums[i];
+//             if (temp_num == 0) {
+//                 max_d[i] = 0;
+//                 continue;
+//             }
+//             while (temp_num != 0) {
+//                 int d = temp_num % 10;
+//                 m = max(m, d);
+//                 temp_num /= 10;
+//             }
+//             max_d[i] = m;
+//         }
+//         int result = -1;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (max_d[i] == max_d[j]) {
+//                     result = max(result, nums[i] + nums[j]);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> max_d(n);
-        for (int i = 0; i < n; i++) {
-            int m = 0;
-            int temp_num = nums[i];
-            if (temp_num == 0) {
-                max_d[i] = 0;
-                continue;
-            }
-            while (temp_num != 0) {
-                int d = temp_num % 10;
-                m = max(m, d);
+        vector<int> max_num_for_digit(10, 0);//suppose 51..5 is maxdigit..so at index 5..51 will get stored
+        int result = -1;
+        for (int num : nums) {
+            int temp_num = num;
+            int d = 0;
+            while (temp_num > 0) {
+                d = max(d, temp_num % 10);
+                if (d == 9) {
+                    break;
+                }
                 temp_num /= 10;
             }
-            max_d[i] = m;
-        }
-        int result = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (max_d[i] == max_d[j]) {
-                    result = max(result, nums[i] + nums[j]);
-                }
+            if (max_num_for_digit[d] != 0) {
+                result = max(result, num + max_num_for_digit[d]);
             }
+            max_num_for_digit[d] = max(max_num_for_digit[d], num);
         }
         return result;
     }
